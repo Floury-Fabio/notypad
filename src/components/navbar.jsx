c887a0b9efc9e8dd9011f8eb270aa3f098ba1812
floury-fabio <floury-fabio@hotmail.fr>
2020-06-27 20:21:03 +0200

    add navbar componen

---
+++ src/components/navbar.jsx
@@ -0,0 +1,20 @@
+import React from 'react';
+
+const Navbar = () => (
+  <nav className="navbar navbar-expand-sm bg-light">
+
+    <ul className="navbar-nav">
+      <li className="nav-item">
+        <a className="nav-link" href="#">Link 1</a>
+      </li>
+      <li class="nav-item">
+        <a class="nav-link" href="#">Link 2</a>
+      </li>
+      <li class="nav-item">
+        <a class="nav-link" href="#">Link 3</a>
+      </li>
+    </ul>
+
+  </nav>
+);
+export default Navbar;
