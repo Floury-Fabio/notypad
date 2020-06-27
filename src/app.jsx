c9b1b4ed84e36b0deb15b6483fe6a559d18de474
floury-fabio <floury-fabio@hotmail.fr>
2020-06-27 23:19:49 +0200

    router

--- src/app.jsx
+++ src/app.jsx
@@ -1,8 +1,12 @@
 import React from 'react';
 import Navbar from 'components/navbar'
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
+
 
 const App = () => (
+  <Router>
   <Navbar />
+  </Router>
 );
 
 export default App;
