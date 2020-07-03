b120780b856b7dcc90df7988c214426173e59cfb
floury-fabio <floury-fabio@hotmail.fr>
2020-07-03 16:00:01 +0200

    add authreducer

---
+++ src/redux/reducers/authReducer.jsx
@@ -0,0 +1,41 @@
+import { authRefresher } from 'helpers/reducersHelpers'
+import {
+  REQUEST, LOGIN_SUCCESS, LOGIN_FAILURE, LOGOUT_SUCCESS,
+} from '../types/authTypes'
+
+const initialState = authRefresher();
+
+const authReducer = (state = initialState, action) => {
+  switch (action.type) {
+    case REQUEST:
+      return {
+        ...state,
+        loading: true,
+      };
+    case LOGIN_SUCCESS:
+      return {
+        ...state,
+        isAuth: true,
+        loading: false,
+        userId: action.userId,
+      };
+    case LOGIN_FAILURE:
+      return {
+        ...state,
+        loading: false,
+        error: action.error,
+      };
+    case LOGOUT_SUCCESS:
+      return {
+        ...state,
+        loading: false,
+        isAuth: false,
+      };
+    default:
+      return {
+        ...state,
+      };
+  }
+};
+
+export default authReducer;
