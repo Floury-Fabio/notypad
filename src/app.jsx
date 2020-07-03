ce38705c4bd9d3a0858e3a31c603a0b87e36fd94
floury-fabio <floury-fabio@hotmail.fr>
2020-07-03 13:44:48 +0200

    add route in app

--- src/app.jsx
+++ src/app.jsx
@@ -1,10 +1,15 @@
 import React from 'react';
-import Navbar from 'components/Navbar';
 import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
+
+import SignIn from 'pages/SignIn';
+import Navbar from 'components/Navbar';
 
 const App = () => (
   <Router>
     <Navbar />
+    <Switch>
+      <Route path="/login" component={SignIn} />
+    </Switch>
   </Router>
 );
 
