1fdd57711dc0834ac8df6d407c79e264a0b5e15c
floury-fabio <floury-fabio@hotmail.fr>
2020-07-03 14:51:43 +0200

    add sign up form

---
+++ src/components/SignUpForm.jsx
@@ -0,0 +1,33 @@
+import React from 'react';
+
+const SignUpForm = () => {
+  const submit = () => {
+
+  };
+
+  return (
+    <form className="form p-4 mt-3 mb-3 rounded" action="/action_page.php" onSubmit={submit}>
+      <div className="form-group">
+        <label htmlFor="email">
+          Email address:
+          <input id="email" type="email" className="form-control" placeholder="Enter email" />
+        </label>
+      </div>
+      <div className="form-group">
+        <label htmlFor="pwd">
+          Password:
+          <input type="password" className="form-control" placeholder="Enter password" id="password" />
+        </label>
+      </div>
+      <div className="form-group form-check">
+        <label htmlFor="rememberMe" className="form-check-label">
+          <input id="rememberMe" className="form-check-input" type="checkbox" />
+          Remember me
+        </label>
+      </div>
+      <button type="submit" className="btn btn-primary">Submit</button>
+    </form>
+  );
+};
+
+export default SignUpForm;
