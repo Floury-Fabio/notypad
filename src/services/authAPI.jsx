a5e50af0752a20b3c3a7511c100c5fe0f3bdf304
floury-fabio <floury-fabio@hotmail.fr>
2020-07-03 17:32:48 +0200

    add call signUp

---
+++ src/services/authAPI.jsx
@@ -0,0 +1,23 @@
+const signUp = ({ email, password }) => {
+  const data = {
+    user: {
+      email,
+      password,
+    },
+  };
+
+  const baseURL = process.env.REACT_APP_API_URL;
+  const endUrl = '/signup.json';
+  const url = baseURL + endUrl;
+
+  const request = {
+    method: 'post',
+    headers: {
+      'Content-Type': 'application/json',
+    },
+    body: JSON.stringify(data),
+  };
+
+  return fetch(url, request);
+};
+export { signUp }
