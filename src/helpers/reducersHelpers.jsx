8fc6047af082eb2b4a63ec8f10dd84100a639bdb
floury-fabio <floury-fabio@hotmail.fr>
2020-07-03 22:54:29 +0200

    setup refresher

--- src/helpers/reducersHelpers.jsx
+++ src/helpers/reducersHelpers.jsx
@@ -1,10 +1,25 @@
-const authRefresher = () => (
-  {
-    loading: false,
-    userId: null,
-    isAuth: false,
-    error: null,
+import Cookies from 'js-cookie';
+import jwtDecode from 'jwt-decode';
+
+const authRefresher = () => {
+  let initialState;
+  if (Cookies.get('token') === undefined || Cookies.get('token') === null) {
+    initialState = {
+      loading: false,
+      userId: null,
+      isAuth: false,
+      error: null,
+    };
+  } else {
+    const decodedToken = jwtDecode(Cookies.get('token'));
+    initialState = {
+      loading: false,
+      userId: decodedToken.sub,
+      isAuth: true,
+      error: null,
+    };
   }
-);
+  return initialState;
+};
 
 export { authRefresher }
