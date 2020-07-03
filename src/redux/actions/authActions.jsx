5e576af9bb1d57e02df1a08fba8da9fb6d067159
floury-fabio <floury-fabio@hotmail.fr>
2020-07-03 16:00:17 +0200

    add authactions

---
+++ src/redux/actions/authActions.jsx
@@ -0,0 +1,33 @@
+import {
+  REQUEST, LOGIN_SUCCESS, LOGIN_FAILURE, LOGOUT_SUCCESS,
+} from '../types/authTypes'
+
+const request = () => (
+  {
+    type: REQUEST,
+  }
+);
+
+const loginSuccess = (decodedToken) => (
+  {
+    type: LOGIN_SUCCESS,
+    userType: decodedToken.scp,
+  }
+);
+
+const loginFailure = (error) => (
+  {
+    type: LOGIN_FAILURE,
+    error,
+  }
+);
+
+const logoutSuccess = () => (
+  {
+    type: LOGOUT_SUCCESS,
+  }
+);
+
+export {
+  request, loginSuccess, loginFailure, logoutSuccess,
+};
