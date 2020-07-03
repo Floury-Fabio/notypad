0cb4888c47f65da83c2f034c0b6f3c4e04907bb1
floury-fabio <floury-fabio@hotmail.fr>
2020-07-03 14:53:27 +0200

    add route signup

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,7 @@ import React from 'react';
 import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 
 import SignIn from 'pages/SignIn';
+import SignUp from 'pages/SignUp';
 import Navbar from 'components/Navbar';
 
 const App = () => (
@@ -9,6 +10,7 @@ const App = () => (
     <Navbar />
     <Switch>
       <Route path="/login" component={SignIn} />
+      <Route path="/register" component={SignUp} />
     </Switch>
   </Router>
 );
