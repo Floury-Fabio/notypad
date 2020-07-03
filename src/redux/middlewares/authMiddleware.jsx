e4ceeb3c5ae318eb5adf7b500924e8c384478437
floury-fabio <floury-fabio@hotmail.fr>
2020-07-03 17:22:26 +0200

    add auth middlewares

---
+++ src/redux/middlewares/authMiddleware.jsx
@@ -0,0 +1,38 @@
+import Cookies from 'js-cookie';
+import jwt_decode from 'jwt-decode';
+import * as authAPI from 'services/authAPI';
+import { request, loginSuccess, loginFailure } from 'redux/actions/authActions';
+
+const signUp = (email, password) => (
+  async (dispatch) => {
+    dispatch(request());
+    const response = await authAPI.signUp(email, password);
+    const body = await response.json();
+
+    if (response.status !== 201) {
+      dispatch(loginFailure(body.errors));
+    } else {
+      Cookies.set('token', response.headers.get('Authorization'), { sameSite: 'lax' });
+      const decodedToken = jwt_decode(response.headers.get('Authorization'));
+      dispatch(loginSuccess(decodedToken));
+    }
+  }
+);
+
+const signIn = (email, password, type) => (
+  async (dispatch) => {
+    dispatch(request());
+    const response = await authAPI.signIn(email, password, type);
+    const body = await response.json();
+
+    if (response.status !== 201) {
+      dispatch(loginFailure(body.error));
+    } else {
+      Cookies.set('token', response.headers.get('Authorization'), { sameSite: 'lax' });
+      const decodedToken = jwt_decode(response.headers.get('Authorization'));
+      dispatch(loginSuccess(decodedToken));
+    }
+  }
+);
+
+export { signIn, signUp };
