75759754efd682addd59dc84ecf0826b097a30bd
floury-fabio <floury-fabio@hotmail.fr>
2020-07-03 19:38:00 +0200

    add request in sign in form

--- src/components/SignInForm.jsx
+++ src/components/SignInForm.jsx
@@ -1,22 +1,34 @@
 import React from 'react';
+import { useHistory } from 'react-router-dom';
+import { useDispatch } from 'react-redux';
+import { signUp } from 'redux/middlewares/authMiddlewares';
+import { useInputChange } from 'customHooks/useInputChange';
 
 const SignInForm = () => {
-  const submit = () => {
+  const [input, handleInputChange] = useInputChange();
+  const history = useHistory();
+  const dispatch = useDispatch();
 
+  const submit = async (e) => {
+    e.preventDefault();
+    const code = await dispatch(signUp(input));
+    if (code === 201) {
+      history.push('/home');
+    }
   };
 
   return (
-    <form className="form p-4 mt-3 mb-3 rounded" action="/action_page.php" onSubmit={submit}>
+    <form className="form p-4 mt-3 mb-3 rounded" onSubmit={submit}>
       <div className="form-group">
         <label htmlFor="email">
           Email address:
-          <input id="email" type="email" className="form-control" placeholder="Enter email" />
+          <input id="email" name="email" type="email" className="form-control" placeholder="Enter email" onChange={handleInputChange} />
         </label>
       </div>
       <div className="form-group">
         <label htmlFor="pwd">
           Password:
-          <input type="password" className="form-control" placeholder="Enter password" id="password" />
+          <input id="password" name="password" type="password" className="form-control" placeholder="Enter password" onChange={handleInputChange} />
         </label>
       </div>
       <div className="form-group form-check">
