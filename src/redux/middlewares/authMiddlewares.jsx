93e3444d7f88ec3c341db50902730d279db05f39
floury-fabio <floury-fabio@hotmail.fr>
2020-07-04 11:09:30 +0200

    setup middlewzre for logout

--- src/redux/middlewares/authMiddlewares.jsx
+++ src/redux/middlewares/authMiddlewares.jsx
@@ -1,7 +1,9 @@
 import Cookies from 'js-cookie';
 import jwtDecode from 'jwt-decode';
 import * as authAPI from 'services/authAPI';
-import { request, loginSuccess, loginFailure } from 'redux/actions/authActions';
+import {
+  request, loginSuccess, logoutSuccess, requestFailure,
+} from 'redux/actions/authActions';
 
 const signUp = ({ email, password }) => (
   async (dispatch) => {
@@ -10,7 +12,7 @@ const signUp = ({ email, password }) => (
     const body = await response.json();
 
     if (response.status !== 201) {
-      dispatch(loginFailure(body.errors));
+      dispatch(requestFailure(body.errors));
     } else {
       Cookies.set('token', response.headers.get('Authorization'), { sameSite: 'lax' });
       const decodedToken = jwtDecode(response.headers.get('Authorization'));
@@ -27,7 +29,7 @@ const signIn = ({ email, password }) => (
     const body = await response.json();
 
     if (response.status !== 201) {
-      dispatch(loginFailure(body.error));
+      dispatch(requestFailure(body.error));
     } else {
       Cookies.set('token', response.headers.get('Authorization'), { sameSite: 'lax' });
       const decodedToken = jwtDecode(response.headers.get('Authorization'));
@@ -37,4 +39,20 @@ const signIn = ({ email, password }) => (
   }
 );
 
-export { signIn, signUp };
+const signOut = () => (
+  async (dispatch) => {
+    dispatch(request());
+    const response = await authAPI.signOut();
+    const body = await response.json();
+
+    console.log(response.status)
+    if (response.status !== 204) {
+      dispatch(requestFailure(body.error));
+    } else {
+      Cookies.remove('token');
+      dispatch(logoutSuccess());
+    }
+  }
+);
+
+export { signIn, signUp, signOut };
