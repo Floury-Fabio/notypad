de971719a12483efd1d22bd445123ecaf717a062
floury-fabio <floury-fabio@hotmail.fr>
2020-07-04 16:30:19 +0200

    setup notepads page

---
+++ src/pages/Notepads.jsx
@@ -0,0 +1,18 @@
+import React, { useEffect } from 'react';
+import * as notepadAPI from 'services/notepadAPI';
+import { useSelector } from 'react-redux';
+
+const Notepads = () => {
+  const userId = useSelector((state) => state.authReducer.userId);
+
+  useEffect(async () => {
+    const notepads = await notepadAPI.getNotepads(userId);
+    console.log(notepads)
+  });
+
+  return (
+    <h3> Notepads </h3>
+  );
+};
+
+export default Notepads;
