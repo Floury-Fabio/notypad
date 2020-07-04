28c7966384b801c7f0dfb73c5071b2173b9283ef
floury-fabio <floury-fabio@hotmail.fr>
2020-07-04 10:36:20 +0200

    add logout in navbar

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,24 +1,36 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
+import Cookies from 'js-cookie';
+import { signOut } from 'redux/middlewares/authMiddlewares';
+import { useDispatch } from 'react-redux';
 
-const Navbar = () => (
-  <nav className="navbar navbar-expand-sm bg-light">
+const Navbar = () => {
+  const dispatch = useDispatch();
+  const logout = async () => {
+    const code = await dispatch(signOut(input));
+    console.log(code)
+  };
 
-    <ul className="navbar-nav">
-      <li className="nav-item">
-        <Link to="/home" className="nav-link"> Home </Link>
-      </li>
-      <li className="nav-item">
-        <Link to="/notepads" className="nav-link"> Notepads </Link>
-      </li>
-      <li className="nav-item">
-        <Link to="/login" className="nav-link"> Login </Link>
-      </li>
-      <li className="nav-item">
-        <Link to="/register" className="nav-link"> Register </Link>
-      </li>
-    </ul>
-
-  </nav>
-);
+  return (
+    <nav className="navbar navbar-expand-sm bg-light">
+      <ul className="navbar-nav">
+        <li className="nav-item">
+          <Link to="/home" className="nav-link"> Home </Link>
+        </li>
+        <li className="nav-item">
+          <Link to="/notepads" className="nav-link"> Notepads </Link>
+        </li>
+        <li className="nav-item">
+          <Link to="/login" className="nav-link"> Login </Link>
+        </li>
+        <li className="nav-item">
+          <Link to="/register" className="nav-link"> Register </Link>
+        </li>
+        <li className="nav-item">
+          <button type="button" className="btn btn-primary" onClick={logout}> Logout </button>
+        </li>
+      </ul>
+    </nav>
+  );
+};
 export default Navbar;
