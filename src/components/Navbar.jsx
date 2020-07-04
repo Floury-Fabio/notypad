c4756ec7f63b005f3727e13841e08bc6bcb31169
floury-fabio <floury-fabio@hotmail.fr>
2020-07-04 12:02:01 +0200

    add conditinal renderin on navbar

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,11 +1,12 @@
 import React from 'react';
 import { Link, useHistory } from 'react-router-dom';
 import { signOut } from 'redux/middlewares/authMiddlewares';
-import { useDispatch } from 'react-redux';
+import { useDispatch, useSelector } from 'react-redux';
 
 const Navbar = () => {
   const dispatch = useDispatch();
   const history = useHistory();
+  const isAuth = useSelector((state) => state.authReducer.isAuth);
 
   const logout = async () => {
     const code = await dispatch(signOut());
@@ -20,18 +21,26 @@ const Navbar = () => {
         <li className="nav-item">
           <Link to="/home" className="nav-link"> Home </Link>
         </li>
-        <li className="nav-item">
-          <Link to="/notepads" className="nav-link"> Notepads </Link>
-        </li>
-        <li className="nav-item">
-          <Link to="/login" className="nav-link"> Login </Link>
-        </li>
-        <li className="nav-item">
-          <Link to="/register" className="nav-link"> Register </Link>
-        </li>
-        <li className="nav-item">
-          <button type="button" className="btn btn-primary" onClick={logout}> Logout </button>
-        </li>
+        { isAuth && (
+          <>
+            <li className="nav-item">
+              <Link to="/notepads" className="nav-link"> Notepads </Link>
+            </li>
+            <li className="nav-item">
+              <button type="button" className="btn btn-primary" onClick={logout}> Logout </button>
+            </li>
+          </>
+        )}
+        { !isAuth && (
+          <>
+            <li className="nav-item">
+              <Link to="/login" className="nav-link"> Login </Link>
+            </li>
+            <li className="nav-item">
+              <Link to="/register" className="nav-link"> Register </Link>
+            </li>
+          </>
+        )}
       </ul>
     </nav>
   );
