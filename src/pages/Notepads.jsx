7798c7332a5ef3b2e1cc3b96ae6b85885560c2c2
floury-fabio <floury-fabio@hotmail.fr>
2020-07-05 11:26:03 +0200

    add conditional rendering on notepads page

--- src/pages/Notepads.jsx
+++ src/pages/Notepads.jsx
@@ -1,16 +1,24 @@
-import React, { useEffect } from 'react';
+import React, { useState, useEffect } from 'react';
 import * as notepadAPI from 'services/notepadAPI';
 
 const Notepads = () => {
+  const [notepads, setNotepads] = useState([]);
+
   const fetchNotepads = async () => {
     const response = await notepadAPI.getNotepads();
-    console.log(await response.json())
+    const body = await response.json();
+    setNotepads(body);
   };
 
   useEffect(() => { fetchNotepads(); }, []);
 
   return (
-    <h3> Notepads </h3>
+    <>
+      <h3> Notepads </h3>
+      { notepads.length > 0
+        ? <p> dedede </p>
+        : <p> You haven&apos;t notepads </p> }
+    </>
   );
 };
 
