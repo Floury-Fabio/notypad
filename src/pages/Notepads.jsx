1f2a47f7e077fec005291c1ed0057c30d1ce793b
floury-fabio <floury-fabio@hotmail.fr>
2020-07-08 22:04:44 +0200

    link create modal with notepads page

--- src/pages/Notepads.jsx
+++ src/pages/Notepads.jsx
@@ -1,8 +1,11 @@
 import React, { useState, useEffect } from 'react';
 import * as notepadAPI from 'services/notepadAPI';
 
+import NotepadCreateModal from 'components/NotepadCreateModal';
+
 const Notepads = () => {
   const [notepads, setNotepads] = useState([]);
+  const [show, setShow] = useState(false);
 
   const fetchNotepads = async () => {
     const response = await notepadAPI.getNotepads();
@@ -18,6 +21,8 @@ const Notepads = () => {
       { notepads.length > 0
         ? <p> dedede </p>
         : <p> You haven&apos;t notepads </p> }
+      <button type="button" className="btn btn-primary" onClick={() => setShow(true)}> New Notepad </button>
+      <NotepadCreateModal show={show} onHide={() => setShow(false)} />
     </>
   );
 };
