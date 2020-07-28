71b3157455611508fb3f00855d50f40e0679bd2a
floury-fabio <floury-fabio@hotmail.fr>
2020-07-28 17:24:05 +0200

    add notepadMidllewares

---
+++ src/redux/middlewares/notepadMiddlewares.jsx
@@ -0,0 +1,35 @@
+import * as notepadAPI from 'services/notepadAPI';
+import { request, requestSuccess, requestFailure } from 'redux/actions/requestActions';
+
+const getNotepads = () => (
+  async (dispatch) => {
+    dispatch(request());
+    const response = await notepadAPI.getNotepads();
+    const body = await response.json();
+
+    if (response.status !== 201) {
+      dispatch(requestFailure(body.error));
+    } else {
+      dispatch(requestSuccess());
+    }
+    return response.status;
+  }
+);
+
+const createNotepad = ({ title, userId }) => (
+  async (dispatch) => {
+    dispatch(request());
+    const response = await notepadAPI.createNotepad({ title, userId });
+    const body = await response.json();
+    console.log(body)
+
+    if (response.status !== 201) {
+      dispatch(requestFailure(body.error));
+    } else {
+      dispatch(requestSuccess());
+    }
+    return response.status;
+  }
+);
+
+export { getNotepads, createNotepad };
