53119e55682cf1c6e69aa29ed729fbf58fe541be
Floury-Fabio <floury-fabio@hotmail.fr>
2020-08-11 20:23:47 +0200

    add message display in app.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
+import { useSelector, useDispatch } from 'react-redux';
 
 import SignIn from 'pages/SignIn';
 import SignUp from 'pages/SignUp';
@@ -8,16 +9,39 @@ import NotepadsPage from 'pages/NotepadsPage';
 
 import Navbar from 'components/Navbar';
 
-const App = () => (
-  <Router>
-    <Navbar />
-    <Switch>
-      <Route path="/login" component={SignIn} />
-      <Route path="/register" component={SignUp} />
-      <Route path="/notepads" component={NotepadsPage} />
-      <Route path="/home" component={Home} />
-    </Switch>
-  </Router>
-);
+import { cleanError } from 'redux/actions/requestActions';
+import Alert from 'react-bootstrap/Alert';
+
+const App = () => {
+  const dispatch = useDispatch();
+  const error = useSelector((state) => state.requestReducer.error);
+
+  const errorMessage = () => {
+    let content;
+    if (error !== null) {
+      content = (
+        <Alert variant="danger" onClose={() => dispatch(cleanError())} dismissible>
+          <p>
+            {error}
+          </p>
+        </Alert>
+      );
+    }
+    return content;
+  };
+
+  return (
+    <Router>
+      { errorMessage() }
+      <Navbar />
+      <Switch>
+        <Route path="/login" component={SignIn} />
+        <Route path="/register" component={SignUp} />
+        <Route path="/notepads" component={NotepadsPage} />
+        <Route path="/home" component={Home} />
+      </Switch>
+    </Router>
+  );
+}
 
 export default App;
