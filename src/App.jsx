0c5f15772d231283525d63aff1af3a10a1d8b1c5
Floury-Fabio <floury-fabio@hotmail.fr>
2020-08-14 21:58:04 +0200

    replace alert error function by alert component

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,5 @@
 import React from 'react';
 import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
-import { useSelector, useDispatch } from 'react-redux';
 
 import SignIn from 'pages/SignIn';
 import SignUp from 'pages/SignUp';
@@ -8,40 +7,19 @@ import Home from 'pages/Home';
 import NotepadsPage from 'pages/NotepadsPage';
 
 import Navbar from 'components/Navbar';
+import AlertComponent from 'components/AlertComponent';
 
-import { cleanError } from 'redux/actions/requestActions';
-import Alert from 'react-bootstrap/Alert';
-
-const App = () => {
-  const dispatch = useDispatch();
-  const error = useSelector((state) => state.requestReducer.error);
-
-  const errorMessage = () => {
-    let content;
-    if (error !== null) {
-      content = (
-        <Alert variant="danger" onClose={() => dispatch(cleanError())} dismissible>
-          <p>
-            {error}
-          </p>
-        </Alert>
-      );
-    }
-    return content;
-  };
-
-  return (
-    <Router>
-      { errorMessage() }
-      <Navbar />
-      <Switch>
-        <Route path="/login" component={SignIn} />
-        <Route path="/register" component={SignUp} />
-        <Route path="/notepads" component={NotepadsPage} />
-        <Route path="/home" component={Home} />
-      </Switch>
-    </Router>
-  );
-}
+const App = () => (
+  <Router>
+    <AlertComponent />
+    <Navbar />
+    <Switch>
+      <Route path="/login" component={SignIn} />
+      <Route path="/register" component={SignUp} />
+      <Route path="/notepads" component={NotepadsPage} />
+      <Route path="/home" component={Home} />
+    </Switch>
+  </Router>
+);
 
 export default App;
