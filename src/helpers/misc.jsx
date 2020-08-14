ab2112b49667011920971888d830bbd44280fd9f
Floury-Fabio <floury-fabio@hotmail.fr>
2020-08-14 22:30:57 +0200

    add object checking in adaptErrormessage

--- src/helpers/misc.jsx
+++ src/helpers/misc.jsx
@@ -1,7 +1,10 @@
 import React from 'react';
 
 const adaptErrorMessage = (errors) => {
-  console.log(Object.entries(errors));
+  if (!(errors instanceof Object)) {
+    return errors;
+  }
+
   let message = '';
   Object.entries(errors).forEach(([key, value]) => {
     value.forEach((el) => {
