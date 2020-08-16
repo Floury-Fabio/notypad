62f51420375052a021d92d35a5d4a5df4838b2a6
Floury-Fabio <floury-fabio@hotmail.fr>
2020-08-16 18:55:17 +0200

    add call modal to updbutton in notepadrow

--- src/components/NotepadRow.jsx
+++ src/components/NotepadRow.jsx
@@ -1,20 +1,37 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import PropTypes from 'prop-types';
 import Button from 'react-bootstrap/Button';
+import NotepadModal from 'components/NotepadModal';
 
-const NotepadRow = ({ notepad }) => (
-  <tr>
-    <td>{notepad.title}</td>
-    <td width="10%">
-      <Button variant="primary">upd</Button>
-    </td>
-  </tr>
-);
+const NotepadRow = ({ notepad }) => {
+  const [show, setShow] = useState(false);
+  const [data, setData] = useState({ notepadId: notepad.id, title: notepad.title });
 
+  const changeTitle = async () => {
+    if (data.title !== notepad.title) {
+      console.log(data)
+    }
+  };
+
+  useEffect(() => { changeTitle(); }, [data]);
+
+  return (
+    <>
+      <tr>
+        <td>{notepad.title}</td>
+        <td width="10%">
+          <Button variant="primary" onClick={() => setShow(true)}>upd</Button>
+        </td>
+      </tr>
+      <NotepadModal show={show} onHide={() => setShow(false)} data={data} setData={setData} />
+    </>
+  );
+};
 export default NotepadRow;
 
 NotepadRow.propTypes = {
   notepad: PropTypes.shape({
+    id: PropTypes.number,
     title: PropTypes.string,
   }).isRequired,
 };
