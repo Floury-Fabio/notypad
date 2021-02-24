ad423559847e8f6f4bcba6ad8499743aa2f3b5a9
floury-fabio <floury-fabio@hotmail.fr>
2021-02-24 17:55:23 +0100

    add updateNotepadsList action in notepads midlleware and add try catch for error

--- src/redux/middlewares/notepadMiddlewares.jsx
+++ src/redux/middlewares/notepadMiddlewares.jsx
@@ -1,34 +1,42 @@
+import Cookies from 'js-cookie';
+
 import * as notepadAPI from 'services/notepadAPI';
 import { request, requestSuccess, requestFailure } from 'redux/actions/requestActions';
+import updateNotepadsList from 'redux/actions/notepadActions';
 
-const getNotepads = () => (
-  async (dispatch) => {
+const getNotepads = () => async (dispatch) => {
+  try {
     dispatch(request());
     const response = await notepadAPI.getNotepads();
     const body = await response.json();
 
-    if (response.status !== 201) {
-      dispatch(requestFailure(body.error));
-    } else {
-      dispatch(requestSuccess());
-    }
-    return response.status;
-  }
-);
+    if (!response.ok) { throw new Error(body.error); }
 
-const createNotepad = ({ title, userId }) => (
-  async (dispatch) => {
+    dispatch(updateNotepadsList(body));
+    Cookies.set('notepadsList', body, { sameSite: 'lax' });
+
+    dispatch(requestSuccess());
+    return true;
+  } catch (errorMessage) {
+    dispatch(requestFailure(errorMessage));
+    return false;
+  }
+};
+
+const createNotepad = ({ title, userId }) => async (dispatch) => {
+  try {
     dispatch(request());
     const response = await notepadAPI.createNotepad({ title, userId });
     const body = await response.json();
 
-    if (response.status !== 201) {
-      dispatch(requestFailure(body.error));
-    } else {
-      dispatch(requestSuccess());
-    }
-    return response.status;
+    if (!response.ok) { throw new Error(body.error); }
+
+    dispatch(requestSuccess());
+    return true;
+  } catch (errorMessage) {
+    dispatch(requestFailure(errorMessage));
+    return false;
   }
-);
+};
 
 export { getNotepads, createNotepad };
