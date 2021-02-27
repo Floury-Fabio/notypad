8c71c62e0911088e0c98996fe81158aca2348f16
floury-fabio <floury-fabio@hotmail.fr>
2021-02-27 16:52:19 +0100

    rewrite rewrite NoteRow to adapt at current note reducer

--- src/components/NoteRow.jsx
+++ src/components/NoteRow.jsx
@@ -2,24 +2,35 @@ import React from 'react';
 import PropTypes from 'prop-types';
 import { useDispatch } from 'react-redux';
 import { showNotepad as reloadCurrentNotepad } from 'redux/middlewares/notepadMiddlewares';
-import { destroyNote } from 'redux/middlewares/noteMiddlewares';
+import { getNote as reloadCurrentNote, destroyNote } from 'redux/middlewares/noteMiddlewares';
+
 import { BsXCircle } from 'react-icons/bs';
 import { IconContext } from 'react-icons';
 
 const NoteRow = ({ note }) => {
   const dispatch = useDispatch();
-  const remove = async () => {
-    const response = await dispatch(destroyNote({ notepadId: note.notepad_id, noteId: note.id }));
-    if (response) { dispatch(reloadCurrentNotepad({ notepadId: note.notepad_id })); }
+
+  const selectNote = async (event) => {
+    if (event.keyCode === 229 || event.type === 'click') {
+      dispatch(reloadCurrentNote({ notepadId: note.notepad_id, noteId: note.id }));
+    }
   };
+
+  const removeNote = async (event) => {
+    if (event.keyCode === 229 || event.type === 'click') {
+      const response = await dispatch(destroyNote({ notepadId: note.notepad_id, noteId: note.id }));
+      if (response) { dispatch(reloadCurrentNotepad({ notepadId: note.notepad_id })); }
+    }
+  };
+
   return (
     <>
       <li className="list-group-item d-flex justify-content-between">
-        <p>
+        <div role="button" tabIndex="-1" onClick={selectNote} onKeyDown={selectNote}>
           {note.title}
-        </p>
+        </div>
         <IconContext.Provider value={{ color: 'red', className: 'global-class-name' }}>
-          <div role="button" tabIndex="0" onClick={remove} onKeyDown={remove}>
+          <div role="button" tabIndex="0" onClick={removeNote} onKeyDown={removeNote}>
             <BsXCircle />
           </div>
         </IconContext.Provider>
