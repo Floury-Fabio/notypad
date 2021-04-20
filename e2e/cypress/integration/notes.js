ac3e3a9410ce5f5def1bd959bb073008479f5ed0
floury-fabio <floury-fabio@hotmail.fr>
2021-04-20 17:13:41 +0200

    add e2e test for update note

--- e2e/cypress/integration/notes.js
+++ e2e/cypress/integration/notes.js
@@ -11,7 +11,7 @@ beforeEach(() => {
   cy.fixture('notes/notes').as('fakeNotes');
 });
 
-describe('get notepad', () => {
+describe('get notepad action', () => {
   it('shows all notes', function getNotes() {
     this.fakeNotes.forEach((fakeNote) => {
       cy.createNote({ ...fakeNote, ...{ notepadId: this.notepadId } });
@@ -25,8 +25,8 @@ describe('get notepad', () => {
   });
 });
 
-describe('create note', () => {
-  it('create note correctly', function getNotes() {
+describe('create note action', () => {
+  it('create note correctly', function createNotes() {
     cy.visit(`/notepad/${this.notepadId}`);
     cy.get('#note-title-input').type('banana');
     cy.get('#note-content-input').type('banana is good');
@@ -34,6 +34,25 @@ describe('create note', () => {
 
     cy.get('ul').should('contain', 'banana');
     cy.get('#note-title-display').should('contain', 'banana');
+    cy.get('#note-content-display').should('contain', 'banana is good');
+  });
+});
+
+describe('update note action', () => {
+  it('update note correctly', function updateNote() {
+    this.fakeNotes.forEach((fakeNote) => {
+      cy.createNote({ ...fakeNote, ...{ notepadId: this.notepadId } });
+    });
+
+    cy.visit(`/notepad/${this.notepadId}`);
+
+    cy.contains(this.fakeNotes[1].title).click();
+    cy.get('#note-content-input').clear().type('banana is good');
+    cy.get('#submit').click();
+
+    cy.contains(this.fakeNotes[0].title).click();
+    cy.contains(this.fakeNotes[1].title).click();
+    cy.get('#note-title-display').should('contain', this.fakeNotes[1].title);
     cy.get('#note-content-display').should('contain', 'banana is good');
   });
 });
