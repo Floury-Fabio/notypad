c219f082cbdf8116868800da7232342db1bc79f6
floury-fabio <floury-fabio@hotmail.fr>
2021-04-22 16:32:23 +0200

    fixup to rework alert component

--- src/components/AlertComponent.jsx
+++ src/components/AlertComponent.jsx
@@ -1,7 +1,4 @@
 import React from 'react';
-import { useSelector, useDispatch } from 'react-redux';
-import { cleanError } from 'redux/actions/requestActions';
-import Alert from 'react-bootstrap/Alert';
 
 import NoticeMessage from 'components/NoticeMessage';
 import ErrorMessage from 'components/ErrorMessage';
