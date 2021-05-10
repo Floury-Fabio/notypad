924d50e60154f1235969a6296cf14077c1017dbd
floury-fabio <floury-fabio@hotmail.fr>
2021-05-10 17:35:38 +0200

    add reset buuton in NotesList component
    this button set fields of current note reducer to empty

--- src/components/NotesList.jsx
+++ src/components/NotesList.jsx
@@ -2,12 +2,26 @@ import React from 'react';
 import PropTypes from 'prop-types';
 
 import NoteRow from 'components/NoteRow';
+import { useDispatch } from 'react-redux';
+import { updateCurrentNote } from 'redux/actions/noteActions';
 
-const NotesList = ({ notes }) => (
-  <ul className="w-25 bg-secondary list-group rounded p-1">
-    {notes.map((note) => <NoteRow note={note} key={`${note.title} ${note.id}`} />)}
-  </ul>
-);
+const NotesList = ({ notes }) => {
+  const dispatch = useDispatch();
+  const resetNote = () => {
+    dispatch(updateCurrentNote({
+      title: '',
+      content: '',
+    }));
+  };
+  return (
+    <div className="w-25 bg-secondary rounded p-1 d-flex flex-column">
+      <ul className="list-group w-100">
+        {notes.map((note) => <NoteRow note={note} key={`${note.title} ${note.id}`} />)}
+      </ul>
+      <button id="submit" type="submit" className="btn btn-primary mt-auto ml-auto mb-2" onClick={resetNote}>reset</button>
+    </div>
+  );
+};
 
 export default NotesList;
 
